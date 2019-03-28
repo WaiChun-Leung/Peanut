dffcd663661e942ca50261081e359b5a37f506e3
Wai Chun Leung <725030@student.tor.loc>
2019-03-28 15:18:01 -0400

    I added Download() Method

--- C0725030/C0725030/Program.cs
+++ C0725030/C0725030/Program.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace C0725030
@@ -11,6 +12,13 @@ namespace C0725030
         static void Main(string[] args)
         {
             Console.WriteLine("Peter Rules the IT Universe!");
+            Download();
+            Console.ReadLine();
+        }
+        static void Download()
+        {
+            Thread.Sleep(4000);
+            Console.WriteLine("Download Completed!");
         }
     }
 }
