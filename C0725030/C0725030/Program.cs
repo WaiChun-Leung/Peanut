ee1a14cd8610c712c82ae2aa8330e3668bfb5410
Wai Chun Leung <725030@student.tor.loc>
2019-03-28 15:08:41 -0400

    First Commit

---
+++ C0725030/C0725030/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0725030
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Peter Rules the IT Universe!");
+        }
+    }
+}
